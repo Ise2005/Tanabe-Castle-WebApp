1b44771c94ca2eeb7c8877b26253b55c7427de53
Ise2005 <e9223@g.maizuru-ct.ac.jp>
2024-07-02 15:05:39 +0900

    解

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Tanabe-Castle-WebApp/'
+  base: process.env.GITHUB_PAGES
+      ? '/Tanabe-Castle-WebApp/'
+      : "./"
 })
