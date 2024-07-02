eeb8432327b57544937dbb3a056f71f26e35d931
Ise2005 <e9223@g.maizuru-ct.ac.jp>
2024-07-02 15:46:30 +0900

    k

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter as Router } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-    <Router basename="/Tanabe-Castle-WebApp/">
-      <App />
-    </Router>
-  </React.StrictMode>,
+        <Router basename="/Tanabe-Castle-WebApp/">
+            <App />
+        </Router>
+    </React.StrictMode>,
 )
